8479533c3f0de1f0b77fe39c4c54acbc8c2320a7
Dawid <u56128@student.wat.edu.pl>
2025-06-03 09:57:12 +0200

    Testowanie

---
+++ src/main/java/org/example/demo1/SampleController.java
@@ -0,0 +1,19 @@
+package org.example.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/test")
+    public String someMethod()
+    {
+        return "testowy";
+    }
+    @GetMapping("/GetUserData")
+    public UserData metoda1()
+    {
+        return new UserData(1, "Jan", "Kowalski");
+    }
+
+}
---
+++ src/main/java/org/example/demo1/UserData.java
@@ -0,0 +1,13 @@
+package org.example.demo1;
+
+public class UserData {
+    public int userId;
+    public String imie;
+    public String nazwisko;
+    public UserData(int userId, String imie, String nazwisko)
+    {
+        this.userId = userId;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+}
